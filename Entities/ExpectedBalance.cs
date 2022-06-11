cdb30a89a3efde4ae1097318cbaa367811e997d8
mekk1t <kit.kalimov@gmail.com>
2022-06-11 09:10:43 +0300

    Create ExpectedBalance.cs

---
+++ Entities/ExpectedBalance.cs
@@ -0,0 +1,18 @@
+﻿namespace KitBudget.Entities
+{
+    public class ExpectedBalance
+    {
+        private readonly IEnumerable<Income> _incomes;
+        private readonly IEnumerable<Expense> _expenses;
+        private readonly int _untouchableMoneyAmount;
+
+        public int Amount => _incomes.Sum(i => i.Amount) - _expenses.Sum(e => e.Amount) - _untouchableMoneyAmount;
+
+        public ExpectedBalance(IEnumerable<Income> incomes, IEnumerable<Expense> expenses, int untouchableMoneyAmount)
+        {
+            _incomes = incomes;
+            _expenses = expenses;
+            _untouchableMoneyAmount = untouchableMoneyAmount;
+        }
+    }
+}
