19dbbc6701bd3cb9233984ce41b2cc207982c011
mekk1t <kit.kalimov@gmail.com>
2022-06-11 11:42:58 +0300

    Added DDD classes

---
+++ Entities/Abstractions/Entity.cs
@@ -0,0 +1,56 @@
+﻿using KitBudget.Entities.Exceptions;
+
+namespace KitBudget.Entities.Abstractions
+{
+    /// <summary>
+    /// Базовый класс сущности предметной области.
+    /// </summary>
+    /// <remarks>
+    /// Объекты отличаются идентификатором.
+    /// </remarks>
+    public abstract class Entity
+    {
+        public long Id { get; }
+
+        protected Entity()
+        {
+        }
+
+        protected Entity(long id)
+        {
+            if (id == default)
+                throw new InvariantException("Нельзя создать сущность на основе ID по умолчанию");
+
+            Id = id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Entity other)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (Id.Equals(default) || other.Id.Equals(default))
+                return false;
+
+            return Id.Equals(other.Id);
+        }
+
+        public static bool operator ==(Entity? a, Entity? b)
+        {
+            if (a is null && b is null)
+                return true;
+
+            if (a is null || b is null)
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity? a, Entity? b) => !(a == b);
+
+        public override int GetHashCode() => (GetType().ToString() + Id).GetHashCode();
+    }
+}
---
+++ Entities/Exceptions/InvariantException.cs
@@ -0,0 +1,9 @@
+﻿namespace KitBudget.Entities.Exceptions
+{
+    public class InvariantException : Exception
+    {
+        public InvariantException(string message) : base(message)
+        {
+        }
+    }
+}
