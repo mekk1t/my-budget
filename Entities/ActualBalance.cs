4d518c41bd9794237c38eff30e8a4f00ad55d6b6
mekk1t <kit.kalimov@gmail.com>
2022-06-11 09:26:40 +0300

    Create ActualBalance.cs

---
+++ Entities/ActualBalance.cs
@@ -0,0 +1,19 @@
+﻿namespace KitBudget.Entities
+{
+    public class ActualBalance
+    {
+        private readonly ExpectedBalance _expectedBalance;
+        private readonly IEnumerable<Expense> _actualExpenses;
+        private readonly int _currentAmount;
+
+        public int Delta => _expectedBalance.Amount - _actualExpenses.Sum(e => e.Amount);
+        public int Amount => _currentAmount + Delta;
+
+        public ActualBalance(int currentAmount, ExpectedBalance expectedBalance, IEnumerable<Expense> actualExpenses)
+        {
+            _currentAmount = currentAmount;
+            _expectedBalance = expectedBalance;
+            _actualExpenses = actualExpenses;
+        }
+    }
+}
