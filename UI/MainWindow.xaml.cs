1274068a47282c1642d7f481f1845bcd09409a18
mekk1t <kit.kalimov@gmail.com>
2022-06-11 08:46:25 +0300

    Synced namespace

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace UI
+namespace KitBudget.UI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
