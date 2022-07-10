cfe407855d5b824ded93481a88b5e5a2de4cee3b
mekk1t <kit.kalimov@gmail.com>
2022-07-10 16:20:26 +0300

    Update Calculator.cs

--- Kit.Ledger.Domain/New/Calculator.cs
+++ Kit.Ledger.Domain/New/Calculator.cs
@@ -1,15 +1,40 @@
 ﻿namespace Kit.Ledger.Domain.New
 {
-    public class Calculator
+    public static class Calculator
     {
         private const decimal PERCENTAGE = 0.1m;
 
-        public decimal CalculateUntouchableMoneyDeposit(Revision revision)
+        public static decimal UntouchableMoneyDeposit(Revision revision)
         {
             return revision.Incomes.Sum() * PERCENTAGE;
         }
 
-        public decimal CalculatePocketMoneyDeposit(Budget budget)
+        public static decimal UntouchableMoneyBalanceAtTheStartOfTheMonth(Budget previousMonthBudget, Revision firstRevision)
+        {
+            return
+                previousMonthBudget.FirstRevision.UntouchableMoneyBalance -
+                previousMonthBudget.SecondRevision.UntouchableMoneyBalance +
+                UntouchableMoneyDeposit(firstRevision);
+        }
+
+        public static decimal PocketMoneyBalanceAtTheStartOfTheMonth(Budget previousMonthBudget)
+        {
+            return
+                previousMonthBudget.FirstRevision.PocketMoneyBalance -
+                previousMonthBudget.SecondRevision.PocketMoneyBalance;
+        }
+
+        public static decimal FirstRevisionSberbankTransferAmount(Revision firstRevision)
+        {
+            return firstRevision.Expenses.Sum();
+        }
+
+        /// <summary>
+        /// Сколько денег отложить на счёт "Карманные расходы".
+        /// </summary>
+        /// <param name="budget"></param>
+        /// <returns></returns>
+        public static decimal PocketMoneyDeposit(Budget budget)
         {
             return
                 (
