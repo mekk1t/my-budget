99f047d07f21c6ab4707e9f444e7d9ea0d4fae3e
mekk1t <kit.kalimov@gmail.com>
2022-07-16 13:18:03 +0300

    New entity: Account + AccountReport

---
+++ Kit.Ledger.Domain/New/Account.cs
@@ -0,0 +1,36 @@
+﻿using Kit.Ledger.Domain.Enums;
+
+namespace Kit.Ledger.Domain.New
+{
+    /// <summary>
+    /// Счёт в банке.
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Банк, в котором открыт счёт.
+        /// </summary>
+        public Bank Bank { get; }
+        /// <summary>
+        /// Название счёта.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Владелец счёта.
+        /// </summary>
+        public Person Person { get; }
+
+        /// <summary>
+        /// Создает объект счёта в банке.
+        /// </summary>
+        /// <param name="bank">Банк, в котором открыт счёт.</param>
+        /// <param name="name">Название счёта.</param>
+        /// <param name="person">Владелец счёта.</param>
+        public Account(Bank bank, string name, Person person)
+        {
+            Bank = bank;
+            Name = name;
+            Person = person;
+        }
+    }
+}
---
+++ Kit.Ledger.Domain/New/AccountReport.cs
@@ -0,0 +1,63 @@
+﻿namespace Kit.Ledger.Domain.New
+{
+    /// <summary>
+    /// Ежемесячный отчёт по счету.
+    /// </summary>
+    public class AccountReport
+    {
+        /// <summary>
+        /// Счёт, по которому делается отчёт.
+        /// </summary>
+        public Account Account { get; }
+        /// <summary>
+        /// Месяц, за который делается отчёт.
+        /// </summary>
+        public Month Month { get; }
+        /// <summary>
+        /// Год, за который делается отчёт.
+        /// </summary>
+        public int Year { get; }
+        /// <summary>
+        /// Баланс в начале месяца.
+        /// </summary>
+        public decimal InitialBalance { get; }
+        /// <summary>
+        /// Список доходов.
+        /// </summary>
+        public List<decimal> Incomes { get; }
+        /// <summary>
+        /// Список трат.
+        /// </summary>
+        public List<Expense> Expenses { get; }
+        /// <summary>
+        /// Баланс в конце месяца.
+        /// </summary>
+        public decimal FinalBalance { get; }
+
+        /// <summary>
+        /// Создает отчёт за месяц.
+        /// </summary>
+        /// <param name="account">Счёт, по которому делается отчёт.</param>
+        /// <param name="month">Месяц, за который делается отчёт.</param>
+        /// <param name="initialBalance">Баланс в начале месяца.</param>
+        /// <param name="finalBalance">Баланс в конце месяца.</param>
+        /// <param name="expenses">Список трат за месяц.</param>
+        /// <param name="incomes">Список доходов за месяц.</param>
+        public AccountReport(
+            Account account,
+            Month month,
+            decimal initialBalance,
+            decimal finalBalance,
+            List<Expense> expenses,
+            List<decimal> incomes)
+        {
+            Account = account;
+            Month = month;
+            InitialBalance = initialBalance;
+            FinalBalance = finalBalance;
+            Expenses = expenses;
+            Year = DateTime.Now.Year;
+            Incomes = incomes;
+        }
+    }
+}
