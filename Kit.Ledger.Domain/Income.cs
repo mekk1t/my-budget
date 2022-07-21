f675c6d699fc1634acc0ccbb194e96383ab0a353
mekk1t <kit.kalimov@gmail.com>
2022-07-21 08:25:41 +0300

    Defined Income

---
+++ Kit.Ledger.Domain/Income.cs
@@ -0,0 +1,18 @@
+﻿using Kit.Ledger.Domain.Enums;
+
+namespace Kit.Ledger.Domain
+{
+    public class Income
+    {
+        public decimal Amount { get; }
+        public IncomeType Type { get; }
+        public Person Person { get; }
+
+        public Income(decimal amount, IncomeType type, Person person)
+        {
+            Amount = amount;
+            Type = type;
+            Person = person;
+        }
+    }
+}
---
+++ Kit.Ledger.Domain/IncomeType.cs
@@ -0,0 +1,10 @@
+﻿namespace Kit.Ledger.Domain
+{
+    public enum IncomeType
+    {
+        None = 0,
+        Salary = 1,
+        Gift = 2,
+        Pension = 3
+    }
+}
